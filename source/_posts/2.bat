18573abdc552ab9f11c42bb74bd36aae2c9d0b8c
miemietou <275953187@qq.com>
2021-03-06 11:04:56 +0800

    bat push

--- source/_posts/2.bat
+++
@@ -1,2 +0,0 @@
-start "" "C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe"
-start  http://localhost:4000/
--- source/_posts/RunHexo.bat
+++ source/_posts/RunHexo.bat
@@ -1,1 +1,2 @@
+start  http://localhost:4000/
 cmd /k "cd /d C:\Users\tsui\blog&&hexo s"
